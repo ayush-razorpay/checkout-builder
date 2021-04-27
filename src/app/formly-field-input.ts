1b2c89b0bbe65c03fe967961aabdbed074b185e5
Ayush Vipul <ayush.vipul@razorpay.com>
2021-04-27 10:21:46 +0530

    some changes

--- src/app/formly-field-input.ts
+++ src/app/formly-field-input.ts
@@ -4,7 +4,7 @@ import { FieldType } from '@ngx-formly/core';
 @Component({
  selector: 'formly-field-input',
  template: `
- <label for="formly_16_input_currency_1"> Color Picker <span>*</span><!--bindings={
+ <label for="formly_16_input_currency_1"> Select Theme Color <span>*</span><!--bindings={
     "ng-reflect-ng-if": "true"
   }--></label>
    <input type="color" class="form-control ng-untouched ng-pristine ng-valid"  [formControl]="formControl" [formlyAttributes]="field" >
--- src/app/payment-blocks/payment-block/payment-block.component.ts
+++ src/app/payment-blocks/payment-block/payment-block.component.ts
@@ -1,18 +1,21 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { BlockBuilderServiceService } from 'src/app/block-builder/block-builder-service.service';
-import { PaymentBlockInterface } from '../PaymentBlocksInterface';
+import { PaymentInstrument } from '../PaymentBlockModels';
+
 
 @Component({
   selector: 'app-payment-block',
   templateUrl: './payment-block.component.html',
   styleUrls: ['./payment-block.component.css']
 })
-export class PaymentBlockComponent implements OnInit,PaymentBlockInterface {
+export class PaymentBlockComponent extends PaymentInstrument implements OnInit {
 
 
-  @ViewChild('dynamicSubBlock') dynamicSubBlock: PaymentBlockInterface;
+  @ViewChild('dynamicSubBlock') dynamicSubBlock: PaymentInstrument;
 
-  constructor(private blockBuilderService:BlockBuilderServiceService) { }
+  constructor(private blockBuilderService:BlockBuilderServiceService) {
+    super();
+   }
 
 
 
